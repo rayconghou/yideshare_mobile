450b0166b26411cb24e46d4d74d37163e217632c
nwu698 <nwu698@gmail.com>
2025-09-13 17:39:14 -0400

    added more fonts + changed text colors

---
+++ src/constants/fonts.ts
@@ -0,0 +1,19 @@
+import {Platform} from 'react-native';
+
+export const isIOS = () => {
+  return Platform.OS === 'ios';
+};
+
+export const fontFamilies = {
+//   LEXEND: {
+//     normal: isIOS() ? 'Montserrat-Regular' : 'MontserratRegular',
+//     medium: isIOS() ? 'Montserrat-Medium' : 'MontserratMedium',
+//     bold: isIOS() ? 'Montserrat-Bold' : 'MontserratBold',
+//   },
+//   RUBIK: {
+//     normal: isIOS() ? 'Rubik-Regular' : 'RubikRegular',
+//     medium: isIOS() ? 'Rubik-Medium' : 'RubikMedium',
+//     bold: isIOS() ? 'Rubik-Bold' : 'RubikBold',
+//   },
+  // Adjust the above code to fit your chosen fonts' names
+};
