8c2ef5611a0112d30049cec9e0fd58b4c585709b
nwu698 <nwu698@gmail.com>
2025-09-23 22:27:59 -0400

    added figma color palette

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,9 +1,12 @@
 export const lightColors = {
-    background: '#E9F4F5',
+    backgroundBlue: '#E9F4F5',
     titleText: '#397060',
     text: '#000000',
     primary: '#CDE3DD',
-    secondary: '#839B94'
+    secondary: '#839B94',
+    tertiary: '#397060',
+    stone: '#343330',
+    placeholder_gray: '#555761'
 }
 
 export const darkColors = {
